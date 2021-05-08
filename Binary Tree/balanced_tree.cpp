4994e2154e85c54abcc974fee4236483e0cf56e1
Vishnu Gopal <vishvargops@gmail.com>
2021-05-08 14:04:25 -0500

    commit

---
+++ Binary Tree/balanced_tree.cpp
@@ -0,0 +1,2 @@
+//given a binary tree determine if the tree is balanced or not
+
---
+++ Binary Tree/find_mode.cpp
@@ -0,0 +1,18 @@
+//use a frequesncy map to store count of unique element
+
+// or
+
+//to avoid using another data structure we can make use of in order traversal
+// and keep track of the max count
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <valarray>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello";
+}
